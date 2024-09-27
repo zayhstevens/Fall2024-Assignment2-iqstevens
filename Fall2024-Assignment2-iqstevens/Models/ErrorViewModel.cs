57624a0d05a017d7e80683eafb222192cde17093
zayhstevens <isaiahstevens17@gmail.com>
2024-09-27 00:41:10 -0500

    Delete Fall2024-Assignment2-iqstevens directory, connection errors

--- Fall2024-Assignment2-iqstevens/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fall2024_Assignment2_iqstevens.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
